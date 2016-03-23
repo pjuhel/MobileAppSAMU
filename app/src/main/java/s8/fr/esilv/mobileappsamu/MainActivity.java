521a4d28b5485469aaed5a0a850c9bafc0d73e3f
pjuhel <juhel.pierre76@gmail.com>
2016-03-23 13:29:59 +0100

    Updated interface / put method Scaffolding ready

--- app/src/main/java/s8/fr/esilv/mobileappsamu/MainActivity.java
+++ app/src/main/java/s8/fr/esilv/mobileappsamu/MainActivity.java
@@ -2,6 +2,19 @@ package s8.fr.esilv.mobileappsamu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendstatus(getString(R.string.status1));
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendstatus(getString(R.string.status2));
+            }
+        });
+        Button button3 = (Button) findViewById(R.id.button3);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendstatus(getString(R.string.status3));
+            }
+        });
+        Button button4 = (Button) findViewById(R.id.button4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendstatus(getString(R.string.status4));
+            }
+        });
+    }
+
+    public void sendstatus(final String status){
+        String url = getString(R.string.url);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", "Status not send");
+                    }
+                }
+        ) {
+
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("status", status);
+
+                return params;
+            }
+
+        };
+
+        queue.add(putRequest);
     }
 }
